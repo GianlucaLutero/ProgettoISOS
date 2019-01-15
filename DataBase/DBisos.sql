dc7ceefbee222c23ad01c369107378cf57e4ada1
Enrico Valguarnera <enry_v@hotmail.it>
2019-01-15 17:19:00 +0100

    Database Populated
    - populated DB
    - SQL script for population

--- DataBase/DBisos.sql
+++ DataBase/DBisos.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS invoice CASCADE;
 DROP TABLE IF EXISTS partecipant CASCADE;
 
 CREATE TABLE conference(
-nome varchar(100) NOT NULL,
+name varchar(100) NOT NULL,
 object_conference varchar(100) NOT NULL,
 conference_description varchar(256),
 location varchar(100) NOT NULL,
@@ -12,7 +12,7 @@ price integer NOT NULL,
 conference_date date NOT NULL,
 end_date date NOT NULL,
 reserved integer NOT NULL,
-PRIMARY KEY (nome)
+PRIMARY KEY (name)
 );
 
 CREATE TABLE places(
@@ -20,7 +20,7 @@ name varchar(100) NOT NULL,
 location varchar(100) NOT NULL,
 phone varchar(100) NOT NULL,
 capacity integer NOT NULL,
-PRIMARY KEY (nome)
+PRIMARY KEY (name)
 );
 
 CREATE TABLE invoice(
@@ -28,18 +28,14 @@ name varchar(100) NOT NULL,
 details varchar(256) NOT NULL,
 adress varchar(100) NOT NULL,
 ammount integer NOT NULL,
-PRIMARY KEY (nome)
+PRIMARY KEY (name)
 );
 
 CREATE TABLE partecipant(
-name varchar(100) PRIMARY KEY,
+name varchar(100) NOT NULL,
 surname varchar(100)  NOT NULL,
 job varchar(100) NOT NULL ,
 sex varchar(100) NOT NULL,
 addres varchar(100) NOT NULL,
-PRIMARY KEY (nome, surname)
+PRIMARY KEY  (name, surname)
 );
-
-
-
-
---
+++ DataBase/populate_DB.sql
@@ -0,0 +1,31 @@
+-- Population script
+-- Inserting in places
+INSERT INTO places (name, location, phone, capacity)
+VALUES ('Hotel Palace', 'Palermo', '3243042891', '500');
+
+INSERT INTO places (name, location, phone, capacity)
+VALUES ('Hotel Delle Palme', 'Palermo', '3235428791', '350');
+
+INSERT INTO places (name, location, phone, capacity)
+VALUES ('Sala Conferenze Mazzini', 'Catania', '3883042891', '200');
+
+INSERT INTO places (name, location, phone, capacity)
+VALUES ('Hotel Mario', 'San Benedetto del Tronto', '3243042891', '300');
+
+INSERT INTO places (name, location, phone, capacity)
+VALUES ('Grand Hotel Villa Ada', 'Roma', '3243042891', '280');
+
+INSERT INTO places (name, location, phone, capacity)
+VALUES ('Hotel Barberini', 'Roma', '3243042891', '700');
+
+INSERT INTO places (name, location, phone, capacity)
+VALUES ('Alliance Palace', 'Torino', '3243042891', '550');
+
+INSERT INTO places (name, location, phone, capacity)
+VALUES ('Grand Hotel Garibaldi', 'Milano', '3243042891', '630');
+
+INSERT INTO places (name, location, phone, capacity)
+VALUES ('Fiera', 'Bologna', '3243042891', '200');
+
+INSERT INTO places (name, location, phone, capacity)
+VALUES ('Unicredit Palace', 'Milano', '3243042891', '1000');
